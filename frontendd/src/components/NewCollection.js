4e95421e2bb431628781eff985c14def3888bdbc
Vinay Kumar <138768580+VINAY9129994379@users.noreply.github.com>
2024-09-24 17:38:02 +0530

    Update NewCollection.js

--- frontendd/src/components/NewCollection.js
+++ frontendd/src/components/NewCollection.js
@@ -6,7 +6,7 @@ function NewCollection() {
   const [new_collections, setNew_collections] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/newcollections')
+    fetch('https://eshopping-backend1.onrender.com/newcollections')
       .then((response) => response.json())
       .then((data) => setNew_collections(data))
       .catch((error) => console.error('Error fetching collections:', error));
